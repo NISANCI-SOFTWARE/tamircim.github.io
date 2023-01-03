00a1bf5eb12dd1d672289bc12572050206f2d2c0
Gökmen Nişancı <nisancigokmen@gmail.com>
2023-01-03 15:40:49 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,55 @@
+/*---------------------------------------------*/
+
+/*Design Mobile Header*/
+
+const btnMenuOpen = document.querySelector("#mobile_menu");
+const btnMenuClose = document.querySelector("#mobile_menu_close");
+
+const menu = document.querySelector("#mobile_link");
+
+btnMenuOpen.addEventListener("click", function () {
+  menu.style.display = "block";
+  btnMenuOpen.style.display = "none";
+  btnMenuClose.style.display = "block";
+});
+
+btnMenuClose.addEventListener("click", function () {
+  menu.style.display = "none";
+  btnMenuOpen.style.display = "block";
+  btnMenuClose.style.display = "none";
+});
+
+const clickMobileHome = document.querySelector(".home-mobile");
+clickMobileHome.addEventListener("click", function () {
+  menu.style.display = "none";
+  btnMenuOpen.style.display = "block";
+  btnMenuClose.style.display = "none";
+});
+
+const clickMobileAbout = document.querySelector(".about-mobile");
+clickMobileAbout.addEventListener("click", function () {
+  menu.style.display = "none";
+  btnMenuOpen.style.display = "block";
+  btnMenuClose.style.display = "none";
+});
+
+const clickMobileServcies = document.querySelector(".services-mobile");
+clickMobileServcies.addEventListener("click", function () {
+  menu.style.display = "none";
+  btnMenuOpen.style.display = "block";
+  btnMenuClose.style.display = "none";
+});
+
+const clickMobileComment = document.querySelector(".comment-mobile");
+clickMobileComment.addEventListener("click", function () {
+  menu.style.display = "none";
+  btnMenuOpen.style.display = "block";
+  btnMenuClose.style.display = "none";
+});
+
+const clickMobileContact = document.querySelector(".contact-mobile");
+clickMobileContact.addEventListener("click", function () {
+  menu.style.display = "none";
+  btnMenuOpen.style.display = "block";
+  btnMenuClose.style.display = "none";
+});
